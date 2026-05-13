10ec882b201c05a9fbe7cccc303edc00ac58ed03
D V <dv@Mac.fritz.box>
2026-05-13 23:44:58 +0200

    Initial commit: Kalorien Tracker PWA

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'kcal-v1';
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
+});
